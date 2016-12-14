1296d0585fa364e0901404b4c96aeabd2fb4094c
Kartik Gola <Kartik Gola>
2016-12-15 01:20:42 +0530

    Added home page and bug fix for num input

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:input', function(req, res, next) {
-  
-  var input = req.params.input;
+
+router.get('/', function(req, res, next){
+   res.render('index', { title : "FreeCodeCamp - Timestamp Microservice App"});
+});
+
+router.get('/:input', function (req, res, next) {
   var unix, natural;
-  if (parseInt(input)) { // It is milliseconds
+  var input = req.params.input;
+
+    if (!isNaN(input)) { // It is milliseconds
       var d = new Date();
       var options = { year: 'numeric', month: 'long', day: 'numeric' };
-          
+
       unix = parseInt(input);
       d.setTime(unix);
       natural = d.toLocaleDateString('en-US', options);
-  } 
-  else { // It is date or some random text
-    if (Date.parse(input)){ // It is date 
-      unix = Date.parse(input);
-      natural = input;
-    } else { // It is some random text
-      unix = null;
-      natural = null;
     }
-  }
-
-  res.json({ unix : unix, natural : natural  });
-
+    else { // It is date or some random text
+      if (Date.parse(input)) { // It is date 
+        unix = Date.parse(input);
+        natural = input;
+      } else { // It is some random text
+        unix = null;
+        natural = null;
+      }
+    }
+    res.json({ unix: unix, natural: natural });
 });
 
 module.exports = router;
